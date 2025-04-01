75f960b604592f11aaf6040fc209548e6f024800
noel319 <azeisl917@gmail.com>
2025-04-01 09:14:55 -0400

    set rabbitmq config

---
+++ src/config/rabbitmq.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+module.exports = {
+  url: process.env.RABBITMQ_URL || 'amqp://user:password@rabbitmq:5672',
+  exchanges: {
+    transactions: 'banking.transactions'
+  },
+  queues: {
+    balanceUpdates: 'balance-updates',
+    notifications: 'notifications',
+    auditLog: 'audit-log'
+  }
+};
